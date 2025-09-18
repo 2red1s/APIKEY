cbe0b76d51a1597acb42c88c12784d0bb9b2d788
R E D 1 S O F F <redartik13@gmail.com>
2025-09-18 21:24:56 +0500

    Была добавлена функци запуска эмуляции игр Добавлена функция аутентификации через E-mail и Mobile Guard Исправлена структура и логика кода в SteamKit.cs

--- SteamAPI/SteamKit.cs
+++ SteamAPI/SteamKit.cs
@@ -6,14 +6,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SteamKit2.Internal;
+//using Steamworks;
 
 namespace SteamAPI
 {
     class RedySoft {
+        
         static SteamClient steamClient;
         static CallbackManager manager;
         static SteamUser steamUser;
         static SteamFriends steamFriends;
+
+        static string lastLogin;
+        static string lastPassword;
+        static string authCode;
+
 
         static void Main(string[] args) {
             steamClient = new SteamClient();
@@ -36,34 +43,21 @@ namespace SteamAPI
         static void OnConnected(SteamClient.ConnectedCallback callback)
         {
             Console.WriteLine("Enter your Steam username:");
-            string userlogin = Console.ReadLine();
+            lastLogin = Console.ReadLine();
             Console.WriteLine("Enter your Steam password:");
-            string userpassword = Console.ReadLine();
-            Console.WriteLine("Enter your 2FA code:");
-            string shouldPassword = Console.ReadLine();
+            lastPassword = Console.ReadLine();
+            
 
 
             Console.WriteLine("Connected to Steam!");
 
-            
+
 
             steamUser.LogOn(new SteamUser.LogOnDetails
             {
-                Username = userlogin,
-                Password = userpassword,
-                //TwoFactorCode = "12345", // If you have 2FA enabled, provide the code here
-                TwoFactorCode = shouldPassword,
+                Username = lastLogin,
+                Password = lastPassword
             });
-            //var authSession = await steamClient.Authentication.BeginAuthSessionViaCredentialsAsync(new AuthSessionDetails 
-            //{
-            //    Username = "lowstea__m",
-            //    Password = "78810406Artik",
-            //    IsPersistentSession = shouldRememberPassword,
-
-            //});
-
-
-
 
         }
 
@@ -71,12 +65,40 @@ namespace SteamAPI
 
         static void OnLoggedOn(SteamUser.LoggedOnCallback loggedOnCallback) {
             var playingGame = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
-            if (loggedOnCallback.Result == EResult.OK)
+            
+            if (loggedOnCallback.Result == EResult.AccountLogonDenied)
+            {
+                Console.WriteLine("Please write code from Mail");
+                authCode = Console.ReadLine();
+
+                steamUser.LogOn(new SteamUser.LogOnDetails
+                {   
+                    Username = lastLogin,
+                    Password = lastPassword,
+                    AuthCode = authCode
+                });
+
+            }
+            else if (loggedOnCallback.Result == EResult.AccountLoginDeniedNeedTwoFactor)
+            {
+                Console.WriteLine("Please write code from Steam Guard Mobile Authenticator");
+                authCode = Console.ReadLine();
+
+                steamUser.LogOn(new SteamUser.LogOnDetails
+                {
+                    Username = lastLogin,
+                    Password = lastPassword,
+                    TwoFactorCode = authCode
+                });
+            }
+            else if (loggedOnCallback.Result == EResult.OK)
             {
                 Console.WriteLine("Success Logged to Account");
-                
-                
+                StartGame(730, "CS:GO");
 
+                Console.WriteLine("Press Enter to stop playing the game...");
+                Console.ReadLine();
+                StopGame();
             }
             else
             {
@@ -86,7 +108,30 @@ namespace SteamAPI
 
         }
 
-         
+        static void StartGame(int appId, string gameName = null)
+        {
+            var playingGame = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
+
+            playingGame.Body.games_played.Add(new CMsgClientGamesPlayed.GamePlayed
+            {
+                game_id = 730, // Replace with the desired game's AppID
+                game_extra_info = gameName ?? $"Playing Game {appId}"
+            });
+
+            steamClient.Send(playingGame);
+
+            Console.WriteLine($"Started playing game with AppID: {appId}");
+        }
+
+        static void StopGame()
+        {
+            var playingGame = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
+            // Sending an empty list indicates that the user has stopped playing any game
+            steamClient.Send(playingGame);
+            Console.WriteLine("Stopped playing game.");
+        }
+
+
 
     }
 
